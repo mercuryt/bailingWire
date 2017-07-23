45ea603078e5e540f266e031ff5c32e4e1ff39f3
mercuryT <alexcgilliland@gmail.com>
2017-07-22 17:55:25 -0700

    text, hide, class

--- src/html/chainableSelector.js
+++ src/html/chainableSelector.js
@@ -1,29 +1,44 @@
-function ChainableSelector(selector, scope){
+function ChainableSelector(selector, scope) {
   this.selector = selector;
   this.scope = scope;
 }
 
 ChainableSelector.prototype = {
-  attr: function(attributePath, dataPath){
-     this.scope.bindElement(this.selector, attributePath, dataPath);
-     return this;
+  getElement: function() {
+    return this.scope.template.querySelector(this.selector);
   },
-  template: function(templateName, params){
-     this.scope.bindTemplate(this.selector, templateName, params);
-     return this;
+  attr: function(attributePath, dataPath) {
+    this.scope.bindElement(this.selector, attributePath, dataPath);
+    return this;
   },
-  array: function(templateName, params, dataPath){
+  template: function(templateName, params) {
+    this.scope.bindTemplate(this.selector, templateName, params);
+    return this;
+  },
+  array: function(templateName, params, dataPath) {
     this.scope.bindArray(this.selector, templateName, params, dataPath);
     return this;
   },
-  on: function(eventName, callback){
+  on: function(eventName, callback) {
     this.scope.on(this.selector, eventName, callback);
     return this;
   },
-  focus: function(){
-    this.getElement().focus();
+  focus: function() {
+    this.getElement()
+      .focus();
   },
-  getElement: function(){
-    return this.scope.template.querySelector(this.selector);
+  text: function(dataPath) {
+    return this.attr('innerHTML', dataPath);
+  },
+  hide: function(dataPath) {
+    let get = dataBinding.walkAndGet(this.scope, dataPath);
+    return this.attr('style.display', () => get() ? 'hidden' : 'inital');
+  },
+  class: function(className, dataPaths) {
+    return this.scope.onSet(dataPath, function(x) {
+      let e = this.getElement();
+      if (x) e.classList.add(className);
+      else e.classList.remove(className);
+    });
   }
-}
+};
